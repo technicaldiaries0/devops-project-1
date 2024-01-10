1e694672cdea44de600a6f1a7d42de061eb75d5f
technicaldiaries0 <133200869+technicaldiaries0@users.noreply.github.com>
2024-01-10 12:40:47 +0530

    Update remote_backend_s3.tf
    onlytf

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
     bucket = "dev-proj-1-remote-state-bucket"
-    key    = "devops-project-1/terraform.tfstate"
+    #key    = "devops-project-1/terraform.tfstate"
+    key    = "terraform.tfstate"
     region = "us-east-1"
   }
 }
