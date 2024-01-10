67df86da68c1d21b095d20db914db086b643f801
technicaldiaries0 <133200869+technicaldiaries0@users.noreply.github.com>
2024-01-10 13:07:42 +0530

    Update provider.tf
    /Users/vaibhav/.aws

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,4 +1,4 @@
 provider "aws" {
-  region                   = "eu-central-1"
-  shared_credentials_files = ["/Users/rahulwagh/.aws/credentials"]
+  region                   = "us-east-1a"
+  shared_credentials_files = ["/Users/vaibhav/.aws/credentials"]
 }
